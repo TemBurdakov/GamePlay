12311f3b3e98a51940acc42ce8daabdd073cabbc
TemBurdakov <95189448+TemBurdakov@users.noreply.github.com>
2025-06-17 18:57:28 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,1 @@
-
-document.getElementById('toTop').onclick = function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
+document.getElementById("btn-up").onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' });
